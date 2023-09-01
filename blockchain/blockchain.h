bc25f2052008e794dfb8d87c3994dc0bb5bdb53d
Witut2005 <wiktor.glogowski@onet.pl>
2023-09-01 15:03:19 +0200

    implementing BlockChain std::vector wrapper

---
+++ blockchain/blockchain.h
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+
+template<typename T>
+class BlockChain : public std::vector<T>
+{
+    using riterator =  typename std::vector<T>::reverse_iterator; 
+    using iterator =  typename std::vector<T>::iterator; 
+
+    iterator begin() = delete;
+    riterator rend() = delete;
+
+    public:
+    BlockChain(std::initializer_list<T>) : std::vector<T>(){};
+
+};
